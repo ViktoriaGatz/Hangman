8124b3460e79608cddda90100a3d377f9ca97171
Viki <ledneva.margo"@mail.ru>
2019-05-03 09:54:52 +0700

    One while

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@
 #define PLAY 700
 #define WIN 800
 #define LOSE 900
+#define WAIT_RUS 1000
 /*
 gcc main.c `sdl2-config --libs --cflags` --std=c99 -Wall -lSDL2_image -lm -o
 main
@@ -71,9 +72,37 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_Surface* wait_rus = IMG_Load("./draw/wait_rus.bmp");
+    if (!wait_rus) {
+        printf("error creating cursor\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* texture_wait_rus
+            = SDL_CreateTextureFromSurface(rend, wait_rus);
+    SDL_FreeSurface(wait_rus);
+    if (!texture_wait_rus) {
+        printf("error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Surface* hello = IMG_Load("./draw/hello.bmp");
     if (!hello) {
         printf("error creating hello\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* texture_hello = SDL_CreateTextureFromSurface(rend, hello);
+    SDL_FreeSurface(hello);
+    if (!texture_hello) {
+        printf("error creating texture: %s\n", SDL_GetError());
         SDL_DestroyRenderer(rend);
         SDL_DestroyWindow(win);
         SDL_Quit();
@@ -88,26 +117,6 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
-
-    SDL_Surface* cursor = IMG_Load("./draw/cursor.bmp");
-    if (!cursor) {
-        printf("error creating cursor\n");
-        SDL_DestroyRenderer(rend);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Texture* texture_hello = SDL_CreateTextureFromSurface(rend, hello);
-    SDL_FreeSurface(hello);
-    if (!texture_hello) {
-        printf("error creating texture: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(rend);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return 1;
-    }
-
     SDL_Texture* texture_lang = SDL_CreateTextureFromSurface(rend, lang);
     SDL_FreeSurface(lang);
     if (!texture_lang) {
@@ -118,6 +127,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_Surface* cursor = IMG_Load("./draw/cursor.bmp");
+    if (!cursor) {
+        printf("error creating cursor\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
     SDL_Texture* texture_cursor = SDL_CreateTextureFromSurface(rend, cursor);
     SDL_FreeSurface(cursor);
     if (!texture_cursor) {
@@ -144,12 +161,11 @@ int main(int argc, char* argv[]) {
     int enter = 0;
     int right = 0;
     int left = 1;
-    int norm_cur = 0;
     int pos = -1;
-
+    int state = MENU;
     while (!close_requested) {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
+        if (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_QUIT:
                 close_requested = 1;
@@ -158,50 +174,53 @@ int main(int argc, char* argv[]) {
                 switch (event.key.keysym.scancode) {
                 case SDL_SCANCODE_KP_ENTER:
                 case SDL_SCANCODE_RETURN:
-                    enter = 1;
+                    if (state == MENU) {
+                        state = 200;
+                        enter = 1;
+                    }
+                    if (state == LANGUAGE && pos == 1) {
+                    }
+                    if (state == LANGUAGE && pos == 0) {
+                        state = WAIT_RUS;
+                    }
+                    break;
+                    if (state == LANGUAGE && enter == 1) {
+                    case SDL_SCANCODE_A:
+                    case SDL_SCANCODE_LEFT:
+                        left = 1;
+                        right = 0;
+                        break;
+                    case SDL_SCANCODE_D:
+                    case SDL_SCANCODE_RIGHT:
+                        right = 1;
+                        left = 0;
+                        break;
+                    }
+                default:
                     break;
                 }
-                break;
             }
-            if (enter == 1) {
-                while (!close_requested) {
-                    SDL_Event eevent;
-                    while (SDL_PollEvent(&eevent)) {
-                        switch (eevent.type) {
-                        case SDL_QUIT:
-                            close_requested = 1;
-                            break;
-                        case SDL_KEYDOWN:
-                            switch (eevent.key.keysym.scancode) {
-                            case SDL_SCANCODE_A:
-                            case SDL_SCANCODE_LEFT:
-                                left = 1;
-                                right = 0;
-                                break;
-                            case SDL_SCANCODE_D:
-                            case SDL_SCANCODE_RIGHT:
-                                right = 1;
-                                left = 0;
-                                break;
-                            }
-                            break;
-                        }
-                    }
-                    SDL_RenderClear(rend);
-                    SDL_RenderCopy(rend, texture_lang, NULL, NULL);
-                    if (left == 1 && right == 0) {
-                        ApplySurface(140, 320, texture_cursor, rend);
-                        pos = 0;
-                    }
-                    if (left == 0 && right == 1) {
-                        ApplySurface(360, 320, texture_cursor, rend);
-                        pos = 1;
-                    }
-                    SDL_RenderPresent(rend);
-                    SDL_Delay(1000 / 60);
+            if (state == LANGUAGE) {
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, texture_lang, NULL, NULL);
+                if (left == 1 && right == 0) {
+                    ApplySurface(140, 320, texture_cursor, rend);
+                    pos = 0;
                 }
+                if (left == 0 && right == 1) {
+                    ApplySurface(360, 320, texture_cursor, rend);
+                    pos = 1;
+                }
+                SDL_RenderPresent(rend);
+                SDL_Delay(1000 / 60);
             }
-            if (enter == 0) {
+            if (state == WAIT_RUS) {
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, texture_wait_rus, NULL, NULL);
+                SDL_RenderPresent(rend);
+                SDL_Delay(1000 / 60);
+            }
+            if (state == MENU) {
                 SDL_RenderClear(rend);
                 SDL_RenderCopy(rend, texture_hello, NULL, NULL);
                 SDL_RenderPresent(rend);
@@ -209,6 +228,7 @@ int main(int argc, char* argv[]) {
             }
         }
     }
+
     printf("Done\n");
     atexit(SDL_Quit);
 }
